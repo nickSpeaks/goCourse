6d9098b2b7260581c6a17f6bf34ad4444413f9ef
Nick Andrejeu <andrejeu@gmail.com>
2021-07-18 23:07:19 +0300

    homework 4: interfaces

---
+++ homework04/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+type Rectangle struct {
+	height float64
+	width  float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.height + r.width)
+}
+
+func DescribeShape(s Shape) {
+	fmt.Println(s)
+	fmt.Printf("Area: %.2f\n", s.Area())
+	fmt.Printf("Perimeter: %.2f\n", s.Perimeter())
+}
+
+func main() {
+	c := Circle{radius: 8}
+	r := Rectangle{
+		height: 9,
+		width:  3,
+	}
+	DescribeShape(c)
+	DescribeShape(r)
+}
